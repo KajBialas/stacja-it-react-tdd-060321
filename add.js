05c374a602da132c2f110475903b6b85ca91a240
Kaj Białas <kaj.bialas@dev-industry.com>
2021-03-06 09:47:07 +0100

    init issue0

---
+++ add.js
@@ -0,0 +1,4 @@
+export const add = (a, b) => a + b;
+
+console.log('method ADD should correct add two numbers', add(5,5) === 10);
+console.log('method ADD should correct add two numbers', add(10,5) === 15);
