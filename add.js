58d494f156a88c95a827d19b88acac9101a198ab
Kaj Białas <kaj.bialas@dev-industry.com>
2021-03-06 10:05:31 +0100

    issue0 final

--- add.js
+++ add.js
@@ -1,4 +1,14 @@
-export const add = (a, b) => a + b;
+const add = (a, b) => {
+  if (typeof a === 'number' && typeof b === 'number') {
+    return a + b;
+  } else if (typeof a === 'number' && typeof b === 'undefined') {
+    return a;
+  }
+  return 'NIEPOPRAWNY PARAMETR';
+};
 
 console.log('method ADD should correct add two numbers', add(5,5) === 10);
-console.log('method ADD should correct add two numbers', add(10,5) === 15);
+console.log('method ADD should return number for single param', add(5) === 5);
+console.log('method ADD should return string for wrong param', add('5') === 'NIEPOPRAWNY PARAMETR');
+console.log('method ADD should return string for wrong param', add(5, '5') === 'NIEPOPRAWNY PARAMETR');
+console.log('method ADD should return string for wrong param', add() === 'NIEPOPRAWNY PARAMETR');
