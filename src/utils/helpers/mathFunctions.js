7ab8337ef1c2e239ec138442f9ae103ce724d933
Kaj Białas <kaj.bialas@dev-industry.com>
2021-03-06 11:26:50 +0100

    issue 2 final

--- src/utils/helpers/mathFunctions.js
+++ src/utils/helpers/mathFunctions.js
@@ -6,3 +6,13 @@ export const add = (a, b) => {
   }
   return 'NIEPOPRAWNY PARAMETR';
 };
+
+export const sub = (a,b) => {
+  if (typeof a === 'number' && typeof b === 'number') {
+    return a - b;
+  } else if (typeof a === 'number' && typeof b === 'undefined') {
+    return a;
+  }
+
+  return 'NIEPOPRAWNY PARAMETR';
+}
--- src/utils/helpers/mathFunctions.test.js
+++ src/utils/helpers/mathFunctions.test.js
@@ -1,4 +1,4 @@
-import { add } from './mathFunctions';
+import { add, sub } from './mathFunctions';
 
 describe('mathFunctions.js', () => {
   describe('method ADD()', () => {
@@ -18,6 +18,71 @@ describe('mathFunctions.js', () => {
 
     it('should return string for non params', () => {
       const result = add();
+      const expectedResult = 'NIEPOPRAWNY PARAMETR';
+
+      expect(result).toBe(expectedResult);
+    });
+
+    it('should return string if first param isnt number', () => {
+      const result = add('5', 5);
+      const expectedResult = 'NIEPOPRAWNY PARAMETR';
+
+      expect(result).toBe(expectedResult);
+    });
+
+    it('should return string if second param isnt number', () => {
+      const result = add(5, '5');
+      const expectedResult = 'NIEPOPRAWNY PARAMETR';
+
+      expect(result).toBe(expectedResult);
+    });
+
+    it('should return string if params arent number', () => {
+      const result = add('5', '5');
+      const expectedResult = 'NIEPOPRAWNY PARAMETR';
+
+      expect(result).toBe(expectedResult);
+    });
+  });
+
+  describe('method SUB()', () => {
+    it('should correct sub two numbers', () => {
+      const result = sub(10,5);
+      const expectedResult = 5;
+
+      expect(result).toBe(expectedResult);
+    });
+
+    it('should return number for single param', () => {
+      const result = sub(5);
+      const expectedResult = 5;
+
+      expect(result).toBe(expectedResult);
+    });
+
+    it('should return string for non params', () => {
+      const result = sub();
+      const expectedResult = 'NIEPOPRAWNY PARAMETR';
+
+      expect(result).toBe(expectedResult);
+    });
+
+    it('should return string if first param isnt number', () => {
+      const result = sub('5', 5);
+      const expectedResult = 'NIEPOPRAWNY PARAMETR';
+
+      expect(result).toBe(expectedResult);
+    });
+
+    it('should return string if second param isnt number', () => {
+      const result = sub(5, '5');
+      const expectedResult = 'NIEPOPRAWNY PARAMETR';
+
+      expect(result).toBe(expectedResult);
+    });
+
+    it('should return string if params arent number', () => {
+      const result = sub('5', '5');
       const expectedResult = 'NIEPOPRAWNY PARAMETR';
 
       expect(result).toBe(expectedResult);
