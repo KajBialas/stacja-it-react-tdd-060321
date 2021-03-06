235b0fb79cf72ec846558b2d8af2214a40d8b62a
Kaj Białas <kaj.bialas@dev-industry.com>
2021-03-06 12:28:39 +0100

    issue3 final

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import TopBar from './components/topBar/TopBar';
+import Footer from './components/footer/Footer';
 function App() {
   return (
     <div className="App">
       <TopBar title="Pryzkladowy tekst" />
+      <Footer title="Zajebista firma" />
     </div>
   );
 }
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer({title}) {
+  const currentYear = new Date().getFullYear();
+  return (<div>
+    <span data-testid="footer-copy">{`Copyright ${currentYear}`}</span>
+    <span data-testid="footer-title">{title}</span>
+  </div>)
+}
+
+export default Footer;
